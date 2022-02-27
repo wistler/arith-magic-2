465e596f5b8e17defc423483fa6e441b5b3eb55a
Sooraj Kumar <the.wistler@gmail.com>
2022-02-26 22:54:51 -0500

    handling hydration when store shape changes

--- src/util/persited.ts
+++ src/util/persited.ts
@@ -1,8 +1,16 @@
+import _ from "lodash";
 import { writable, Writable } from "svelte/store";
+
+function mergeState(hydrated: any, def: any): any {
+  if (_.isArray(def) || _.isString(def)) {
+    return hydrated || def;
+  }
+  return { ...def, ...hydrated };
+}
 
 export function persisted<T>(key: string, def: T): Writable<T> {
   const prev = localStorage.getItem(key);
-  const store = writable(prev ? JSON.parse(prev) : def);
+  const store = writable(mergeState(JSON.parse(prev), def));
   store.subscribe((val) => {
     localStorage.setItem(key, JSON.stringify(val));
   });
