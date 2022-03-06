f18fc5bf83837a61da34d2f77a6738c65e6d12b4
Sooraj Kumar <the.wistler@gmail.com>
2022-03-06 00:25:40 -0500

    At least the story renders after removing type def

---
+++ src/Components/WhiteButton.stories.svelte
@@ -0,0 +1,16 @@
+<!-- Button.stories.svelte -->
+<script lang="ts">
+  import { Meta, Story } from "@storybook/addon-svelte-csf";
+
+  import WhiteButton from "./WhiteButton.svelte";
+</script>
+
+<Meta title="Button" component={WhiteButton} />
+
+<Story name="Primary">
+  <WhiteButton>Normal</WhiteButton>
+  <WhiteButton disabled>Disabled</WhiteButton>
+  <WhiteButton flat>Flat</WhiteButton>
+  <WhiteButton flat disabled>Flat Disabled</WhiteButton>
+  <WhiteButton>Icon?</WhiteButton>
+</Story>
